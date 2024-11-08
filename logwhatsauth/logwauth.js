b333052c9c527325a91645df050b24cd84431dfa
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-11-08 08:29:21 +0700

    WHAUTH

--- logwhatsauth/logwauth.js
+++ logwhatsauth/logwauth.js
@@ -5,8 +5,7 @@ import {
   import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
   
   wauthparam.auth_ws = "d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-  wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
-  wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82Mjg5OTk3MTAwNDA/dGV4dD1MT0dJTiBXT0k=";
+  wauthparam.keyword = "  aHR0cHM6Ly93YS5tZS82Mjg1MTU3OTc5NzU5P3RleHQ9d2g0dDVhdXRoMA==";
   wauthparam.tokencookiehourslifetime = 18;
   wauthparam.redirect = "/auth";
   deleteCookie(wauthparam.tokencookiename);
