fae87f9cb93e5361a44264afdc4d7d114826e702
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-12-06 20:20:56 +0700

    3

--- static/js/register.js
+++ static/js/register.js
@@ -1,8 +1,3 @@
-// import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
-// import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.9/element.js";
-
-// addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
-
 // Event listener untuk form submit
 document.getElementById('loginForm').addEventListener('submit', async function (e) {
     e.preventDefault(); // Mencegah submit form secara default
